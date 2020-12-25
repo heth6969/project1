8998c42b7373a3f10f4ad81a34e9d0f1a034748d
heth6969 <76421556+heth6969@users.noreply.github.com>
2020-12-25 14:10:35 +0530

    created code2.py
    positive from a list

---
+++ code2.py
@@ -0,0 +1,14 @@
+
+# list of numbers 
+a =input("Enter first number")
+b =input("Enter second number")
+c =input("Enter third number")
+d =input("Enter fourth number")
+list1=[a,b,c,d]
+print(list1)
+# iterating each number in list 
+for num in list1: 
+	
+	# checking condition 
+	if num >= 0: 
+		print(num, end = " ") 

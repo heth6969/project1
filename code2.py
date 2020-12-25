c0ae462d5970a011e655604426a9b8d0626f8f94
heth6969 <76421556+heth6969@users.noreply.github.com>
2020-12-25 14:26:37 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,13 +1,8 @@
 
-# list of numbers 
-a =input("Enter first number")
-b =input("Enter second number")
-c =input("Enter third number")
-d =input("Enter fourth number")
-list1=[a,b,c,d]
-print(list1)
-# iterating each number in list 
-for num in list1: 
+a=("enter start of range")
+b=("enter end of range")
+
+for num in range(int(a),int(b)+1):
 	
 	# checking condition 
 	if num >= 0: 

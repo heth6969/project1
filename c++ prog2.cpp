894ac98dcf7a4f6d95f92aa1f8a102f8d7d65c2e
heth6969 <76421556+heth6969@users.noreply.github.com>
2021-05-22 14:20:57 +0530

    c++ prog2.cpp
    product

---
+++ c++ prog2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    cout << "Enter two numbers: ";
+
+    // Stores two floating point numbers in variable a and b respectively
+    cin >> a >> b;
+ 
+    // Performs multiplication and stores the result in variable c
+	  c=a*b;  
+
+    cout << "Product = " << c;    
+    
+    return 0;
+}
